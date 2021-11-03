3297d1fb36861b198bd864da025ca6e4169ea945
najlot <najlot@users.noreply.github.com>
2021-11-03 18:56:31 +0100

    async profile handling

--- src/Archive.ClientBase/Services/IProfilesService.cs
+++ src/Archive.ClientBase/Services/IProfilesService.cs
@@ -1,12 +1,13 @@
 ﻿using System.Collections.Generic;
+using System.Threading.Tasks;
 using Archive.ClientBase.Models;
 
 namespace Archive.ClientBase.Services
 {
 	public interface IProfilesService
 	{
-		List<ProfileBase> Load();
-		void Remove(ProfileBase profile);
-		void Save(List<ProfileBase> profiles);
+		Task<List<ProfileBase>> LoadAsync();
+		Task RemoveAsync(ProfileBase profile);
+		Task SaveAsync(List<ProfileBase> profiles);
 	}
 }
